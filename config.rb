8d9165df6642dbfa2a221553216a677016f8cf49
Xander Shepherd <bitnation@alexshepherd.me>
2015-03-28 23:03:32 +0000

    Basic UI in place

--- config.rb
+++ config.rb
@@ -1,5 +1,7 @@
 # Require any additional compass plugins here.
 add_import_path "bower_components/foundation/scss"
+add_import_path "bower_components/font-awesome/scss"
+add_import_path "bower_components/Hover/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(function() {
+	$('#menu-toggle').click(function() {
+		$('#sidebar').toggle();
+		$('.dapp-selector .option').height('80%');
+	});
+});
