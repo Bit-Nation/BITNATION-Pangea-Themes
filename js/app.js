282438a37e24417b4fb3a04aef67f2b4135fa3ab
Xander Shepherd <bitnation@alexshepherd.me>
2015-04-04 02:44:56 +0100

    Animated menu

--- js/app.js
+++ js/app.js
@@ -2,8 +2,24 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-$(function() {
-	$('#menu-toggle').click(function() {
-		$('#sidebar').toggle();
+(function($) {
+	$('#menu-toggle').click(function(a, b, c) {
+		$('#sidebar, #main').toggleClass('sidebarOffScreen');
+
+		$('#sidebar ul li.sidebar-menu-item i.xbnx-icon').each(function(idx, el) {
+			$(el).attr('data-tooltip', '');
+		});
+
+		$('#sidebar ul li.sidebar-menu-item i.xbnx-icon').hover(function(ev) {
+			switch(ev.type) {
+				case 'mouseenter':
+					$(ev.target).attr('data-tooltip', '');
+					break;
+				case 'mouseleave':
+					console.log($(ev.target));
+					console.log('exit');
+					break; 
+			}
+		});
 	});
-});
+})(jQuery);
