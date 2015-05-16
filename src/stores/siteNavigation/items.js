b995031e23b707b84f8a6664ecc0cfee05e994ba
Oskar Dahlberg <oskar.dahlberg@gmail.com>
2015-05-16 23:33:12 +0200

    SiteNav items

--- src/stores/siteNavigation/items.js
+++ src/stores/siteNavigation/items.js
@@ -10,51 +10,18 @@ module.exports = [
     icon: { set: 'fa', type: 'list' }
   },
   {
+    link: 'Encrypted Email',
+    href: '#!/mail',
+    icon: { set: 'fa', type: 'envelope' }
+  },
+  {
     link: 'DApp Library',
-    href: '#!/dapps/library',
-    icon: { set: 'xbnx', type: 'dapp-library' }
-  },
-  {
-    link: 'My DApps',
     href: '#!/dapps',
-    icon: { set: 'xbnx', type: 'my-dapps' }
-  },
-  {
-    link: 'My Data',
-    href: '#!/data',
-    icon: { set: 'xbnx', type: 'my-data' }
+    icon: { set: 'xbnx', type: 'dapp-library' }
   },
   {
     link: 'Services',
     href: '#!/services',
     icon: { set: 'xbnx', type: 'services' }
-  },
-  {
-    link: 'Utilities',
-    href: '#!/utilities',
-    icon: { set: 'xbnx', type: 'utilities' }
-  },
-  {
-    link: 'Profile',
-    icon: { set: 'fa', type: 'user' },
-    items: [
-      {
-        link: 'Encrypted Email',
-        href: '#!/mail'
-      },
-      {
-        link: 'Wallet',
-        href: '#!/wallet'
-      },
-      {
-        link: 'Peers',
-        href: '#!/peers'
-      }
-    ]
-  },
-  {
-    link: 'My Nation',
-    href: '#!/nation',
-    icon: { set: 'fa', type: 'group' }
   }
 ];
